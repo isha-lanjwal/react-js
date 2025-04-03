5893b697825ec7da30b4d8abbdf4ee522990ca30
ishalvss <isha.lanjwal@vishleshan.net>
2025-04-03 15:29:38 +0530

    counter code

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ import './App.css'
 import Card from './component/card.jsx'
 
 function App() {
+  // The useState hook is used in React functional components to manage state. It allows you to store and update values dynamically within a component.
+  const [counter,setCount] = useState(0)
+
+  // if we right setCount(counter+1) multiple times in addValue function, react will consider it as same operation
+  // add will increment counter only by 1 on each click but if we write  setCount(counter => counter + 1) multiple times i.e.3
+  // it will increment counter by 3 on each click as it is function where it takes previous value and update state where it aceept call back and provide last updated state, 
+  // it is a use case of useStatw.
+  const addValue = () => {
+    setCount(counter => counter + 1)
+
+  }
+  const removeValue = () => {
+    if(counter > 0){
+      setCount(counter - 1)
+    }
+  }
   let data = {
     name: 'Erin Lindford',
     designation: 'Product Engineer',
@@ -15,7 +31,11 @@ function App() {
   }
   return (
     <>
-    <h2 className="bg-green-400 text-black p-4 rounded-xl mb-4">Talwind CSS</h2>
+    <h2>Counter Value: {counter}</h2>
+    <button onClick={addValue}>Add Value</button>
+    <br />
+    <button onClick={removeValue}>Remove Value</button>
+    <h2 className="bg-green-400 text-black p-4 rounded-xl mb-4 mt-4">Talwind CSS</h2>
     <Card userData={data} btnText="Message" />
     <Card userData={data2} />
     </>
